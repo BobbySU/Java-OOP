24adbc0c4bb97e4a81008d559f9f389f0cfd1d66
BobbySU <radio_20bg@yahoo.com>
2021-10-30 23:14:49 +0300

    Update scan

--- Working with Abstraction - Lab/src/StudentSystem03/Main.java
+++ Working with Abstraction - Lab/src/StudentSystem03/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         StudentSystem studentSystem = new StudentSystem();
 
         while (true) {
-            String[] input = scanner.nextLine().split(" ");
+            String[] input = scan.nextLine().split(" ");
             if (input[0].equals("Exit")) {
                 break;
             }
