c092bc06f50c53139dc82f9853593ad20d87d3ae
BobbySU <radio_20bg@yahoo.com>
2021-10-31 21:16:34 +0200

    Add Card Rank

---
+++ Working with Abstraction - Exercise/src/CardRank02/Main.java
@@ -0,0 +1,15 @@
+package CardRank02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Card Ranks:");
+
+        for (Cards e : Cards.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", e.ordinal(), e);
+        }
+    }
+}
