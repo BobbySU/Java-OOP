6add63d304bc35e34484fe6d54be08d0cb7b6183
BobbySU <radio_20bg@yahoo.com>
2021-11-01 23:00:32 +0200

    Add Jedi Galaxy

---
+++ Working with Abstraction - Exercise/src/JediGalaxy05/Evil.java
@@ -0,0 +1,22 @@
+package JediGalaxy05;
+
+public class Evil {
+    private int row;
+    private int col;
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
