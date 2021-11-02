fbe95de49dcc0bde56aa19c9daa8e108b45cb08c
BobbySU <radio_20bg@yahoo.com>
2021-11-03 01:10:14 +0200

    Add First And Reserve Team

---
+++ Encapsulation - Lab/src/FirstAndReserveTeam04/Team.java
@@ -0,0 +1,33 @@
+package FirstAndReserveTeam04;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(String name) {
+        this.name = name;
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+    }
+
+    public void addPlayer(Person person) {
+        if (person.getAge() >= 40) {
+            this.reserveTeam.add(person);
+        } else {
+            this.firstTeam.add(person);
+        }
+    }
+
+    public List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(firstTeam);
+    }
+
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(reserveTeam);
+    }
+}
