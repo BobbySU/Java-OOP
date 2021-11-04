00fd915c31a902012d7a9fda9d055b3b7b14c29b
BobbySU <radio_20bg@yahoo.com>
2021-11-04 20:00:31 +0200

    Add Animal Farm

---
+++ Encapsulation - Exercises/src/AnimalFarm02/Main.java
@@ -0,0 +1,20 @@
+package AnimalFarm02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.nextLine();
+        int age = Integer.parseInt(scan.nextLine());
+
+        try {
+            Chicken chicken = new Chicken(name, age);
+            System.out.println(chicken.toString());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
