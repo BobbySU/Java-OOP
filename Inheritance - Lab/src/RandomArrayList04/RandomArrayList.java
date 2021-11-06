4e46189f995fa17a72226de891f7d3539e3257a6
BobbySU <radio_20bg@yahoo.com>
2021-11-07 00:15:11 +0200

    Add Random Array List

---
+++ Inheritance - Lab/src/RandomArrayList04/RandomArrayList.java
@@ -0,0 +1,13 @@
+package RandomArrayList04;
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList {
+
+    public Object getRandomElement() {
+        Random random = new Random();
+        return super.remove(random.nextInt(size()));
+    }
+}
