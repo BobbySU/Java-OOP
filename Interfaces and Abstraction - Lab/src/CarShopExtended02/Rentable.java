33e10bc9d139cc44c1c60c8f1a5f7ae493543e51
BobbySU <radio_20bg@yahoo.com>
2021-11-14 21:45:48 +0200

    Add Car Shop Extended

---
+++ Interfaces and Abstraction - Lab/src/CarShopExtended02/Rentable.java
@@ -0,0 +1,6 @@
+package CarShopExtended02;
+
+public interface Rentable extends Car {
+    Integer getMinRentDay();
+    Double getPricePerDay();
+}
---
+++ Interfaces and Abstraction - Lab/src/CarShopExtended02/Sellable.java
@@ -0,0 +1,5 @@
+package CarShopExtended02;
+
+public interface Sellable extends Car {
+    Double getPrice();
+}
