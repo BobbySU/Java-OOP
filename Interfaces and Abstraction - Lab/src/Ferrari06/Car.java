b57f5443734e52af06e78a503c1b5ec964f2d597
BobbySU <radio_20bg@yahoo.com>
2021-11-15 00:17:07 +0200

    Add Ferrari

---
+++ Interfaces and Abstraction - Lab/src/Ferrari06/Car.java
@@ -0,0 +1,6 @@
+package Ferrari06;
+
+public interface Car {
+    String brakes();
+    String gas();
+}
---
+++ Interfaces and Abstraction - Lab/src/Ferrari06/Main.java
@@ -0,0 +1,12 @@
+package Ferrari06;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Car ferrari = new Ferrari(scan.nextLine());
+        System.out.println(ferrari);
+    }
+}
