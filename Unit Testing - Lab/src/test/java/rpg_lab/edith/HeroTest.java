021eaf03e752c5dae8938828eb225eb01f8d9922
BobbySU <radio_20bg@yahoo.com>
2021-12-04 19:32:10 +0200

    Add Unit Testing - Lab

--- Unit Testing - Lab/src/test/java/rpg_lab/edith/HeroTest.java
+++ Unit Testing - Lab/src/test/java/rpg_lab/edith/HeroTest.java
@@ -1,14 +1,39 @@
 package rpg_lab.edith;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import rpg_lab.edith.interfaces.Target;
+import rpg_lab.edith.interfaces.Weapon;
 
 import static org.junit.Assert.*;
 
 public class HeroTest {
 
-    @Test
-    public void g(){
+    private Hero hero;
 
-        
+    @Before
+    public void setUp() {
+        Weapon mock = Mockito.mock(Weapon.class);
+        this.hero = new Hero("Bobby", mock);
+    }
+
+    @Test
+    public void testUponTargetKillHeroGetXP() {
+        Target mockedTarget =Mockito.mock(Target.class);
+        Mockito.when(mockedTarget.isDead()).thenReturn(true);
+        Mockito.when(mockedTarget.giveExperience()).thenReturn(1000);
+
+        this.hero.attack(mockedTarget);
+        assertEquals(1000,this.hero.getExperience());
+    }
+
+    @Test
+    public void testUponTargetAttackWhileTargetIsStillAliveHeroGetsNoXP() {
+        Target mockedTarget =Mockito.mock(Target.class);
+        Mockito.when(mockedTarget.giveExperience()).thenReturn(100);
+
+        this.hero.attack(mockedTarget);
+        assertEquals(0,this.hero.getExperience());
     }
 }
