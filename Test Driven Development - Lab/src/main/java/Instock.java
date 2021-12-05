8c33d30b56f40cd9ec320319b3d29aa0ded283ed
BobbySU <radio_20bg@yahoo.com>
2021-12-05 18:41:02 +0200

    Add Test Driven Development - Lab

--- Test Driven Development - Lab/src/main/java/Instock.java
+++ Test Driven Development - Lab/src/main/java/Instock.java
@@ -1,66 +1,104 @@
-import jdk.jshell.spi.ExecutionControl;
-
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Instock implements ProductStock {
+    private List<Product> products;
+
+    public Instock() {
+        this.products = new ArrayList<>();
+    }
 
     @Override
     public int getCount() {
-        throw new UnsupportedOperationException();
+        return this.products.size();
     }
 
     @Override
     public boolean contains(Product product) {
-        throw new UnsupportedOperationException();
+        return this.products.contains(product);
     }
 
     @Override
     public void add(Product product) {
-        throw new UnsupportedOperationException();
+        this.products.add(product);
     }
 
     @Override
     public void changeQuantity(String product, int quantity) {
-        throw new UnsupportedOperationException();
+        Product matchedProduct = this.products.stream()
+                .filter(p -> p.getLabel().equals(product))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Product not present"));
+
+        matchedProduct.setQuantity(quantity);
     }
 
     @Override
     public Product find(int index) {
-        throw new UnsupportedOperationException();
+        return this.products.get(index);
     }
 
     @Override
     public Product findByLabel(String label) {
-        throw new UnsupportedOperationException();
+        return this.products.stream()
+                .filter(p -> p.getLabel().equals(label))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Product not present"));
     }
 
     @Override
     public Iterable<Product> findFirstByAlphabeticalOrder(int count) {
-        throw new UnsupportedOperationException();
+        if (count <= 0 || count > this.products.size()) {
+            return new ArrayList<>();
+        }
+        return this.products
+                .stream()
+                .limit(count)
+                .sorted(Comparator.comparing(Product::getLabel))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Product> findAllInRange(double lo, double hi) {
-        throw new UnsupportedOperationException();
+        return this.products
+                .stream()
+                .filter(p -> p.getPrice() > lo && p.getPrice() <= hi)
+                .sorted(Comparator.comparing(Product::getPrice).reversed())
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Product> findAllByPrice(double price) {
-        throw new UnsupportedOperationException();
+        return this.products
+                .stream()
+                .filter(p -> p.getPrice() == price)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Product> findFirstMostExpensiveProducts(int count) {
-        throw new UnsupportedOperationException();
+        if (count > this.products.size()) {
+            throw new IllegalArgumentException("Less products in stock than " + count);
+        }
+        return this.products.stream()
+                .sorted(Comparator.comparing(Product::getPrice).reversed())
+                .limit(count)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Product> findAllByQuantity(int quantity) {
-        throw new UnsupportedOperationException();
+        return this.products
+                .stream()
+                .filter(p -> p.getQuantity() == quantity)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterator<Product> iterator() {
-        throw new UnsupportedOperationException();
+        return this.products.iterator();
     }
 }
